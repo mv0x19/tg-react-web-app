45da840e6174c7f3c864b02ee1504a1d5570008f
mv0x19 <49604532+mv0x19@users.noreply.github.com>
2023-02-12 04:05:12 +0200

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <div className={'header'}>
       <Button onClick={onClose}>Close</Button>
-      <span className={'username'}>{user?.username}</span>
+      <span className={'username'}>{user.username}</span>
     </div>
   );
 };
